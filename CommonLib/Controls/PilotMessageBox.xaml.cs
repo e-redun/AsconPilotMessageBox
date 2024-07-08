06b63bdaa94558d1479836c3a3690305bc92c302
e-redun <e.redun@mail.ru>
2024-07-08 20:23:27 +0500

    Добавьте файлы проекта.

---
+++ CommonLib/Controls/PilotMessageBox.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace CommonLib.Controls
+{
+    public partial class PilotMessageBox
+    {
+        private MessageBoxResult _result = MessageBoxResult.None;
+
+        private static ImageSource _iconSource;
+
+
+        public PilotMessageBox()
+        {
+            InitializeComponent();
+        }
+
+        void AddButtons(MessageBoxButton buttons)
+        {
+            switch (buttons)
+            {
+                case MessageBoxButton.OK:
+                    AddButton("OK", MessageBoxResult.OK, isDefault: true);
+                    break;
+
+                case MessageBoxButton.OKCancel:
+                    AddButton("OK", MessageBoxResult.OK, isDefault: true);
+                    AddButton("Отмена", MessageBoxResult.Cancel, isCancel: true);
+                    break;
+
+                case MessageBoxButton.YesNo:
+                    AddButton("Да", MessageBoxResult.Yes, isDefault: true);
+                    AddButton("Нет", MessageBoxResult.No);
+                    break;
+
+                case MessageBoxButton.YesNoCancel:
+                    AddButton("Да", MessageBoxResult.Yes, isDefault: true);
+                    AddButton("Нет", MessageBoxResult.No);
+                    AddButton("Отмена", MessageBoxResult.Cancel, isCancel: true);
+                    break;
+                default:
+                    throw new ArgumentException("Unknown button value", "buttons");
+            }
+        }
+
+        void AddButton(string text, MessageBoxResult result, bool isCancel = false, bool isDefault = false)
+        {
+            var button = new Button()
+            {
+                Content = text,
+                IsCancel = isCancel,
+                IsDefault = isDefault
+            };
+
+            button.Click += (o, args) =>
+            { 
+                _result = result;
+                DialogResult = true;
+            };
+            
+            ButtonContainer.Children.Add(button);
+        }
+
+
+        public static MessageBoxResult Show(string message,
+                                            string caption,
+                                            MessageBoxButton buttons
+                                            )
+        {
+
+            var dialog = new PilotMessageBox();
+
+            if (!string.IsNullOrWhiteSpace(caption))
+            {
+                dialog.Title = caption;
+            }
+
+            dialog.MessageContainer.Text = message;
+            dialog.AddButtons(buttons);
+
+            if (_iconSource != null)
+            {
+                dialog.Icon.Source = _iconSource;
+                dialog.Icon.Visibility = Visibility.Visible;
+            }
+
+            dialog.ShowDialog();
+
+            return dialog._result;
+        }
+
+
+        public static MessageBoxResult Show(string message,
+                                            string caption = null,
+                                            MessageBoxButton buttons = MessageBoxButton.OK,
+                                            MessageBoxImage image = MessageBoxImage.None
+                                            )
+        {
+            Icon icon = null;
+
+            switch (image)
+            {
+                case MessageBoxImage.None:
+                    break;
+
+                case MessageBoxImage.Hand:
+                    icon = SystemIcons.Hand;
+                    break;
+                
+                case MessageBoxImage.Question:
+                    icon = SystemIcons.Question;
+                    break;
+
+                case MessageBoxImage.Warning:
+                    icon = SystemIcons.Warning;
+                    break;
+
+                case MessageBoxImage.Information:
+                    icon = SystemIcons.Information;
+                    //icon = SystemIcons.N;
+                    break;
+
+                //default: break;
+            };
+
+            if (icon != null)
+            {
+                _iconSource = Imaging.CreateBitmapSourceFromHIcon(
+                        icon.Handle,
+                        Int32Rect.Empty,
+                        BitmapSizeOptions.FromEmptyOptions());
+            }
+
+
+            return Show(message,
+                        caption,
+                        buttons);
+        }
+    }
+}
---
+++ PilotMessageBoxModule/Plugins/MainMenuPlugin.cs
@@ -0,0 +1,33 @@
+﻿using Ascon.Pilot.SDK;
+using Ascon.Pilot.SDK.Menu;
+using CommonLib.Controls;
+using System.ComponentModel.Composition;
+using System.Windows;
+
+namespace PilotMessageBoxModule
+{
+    [Export(typeof(IMenu<MainViewContext>))]
+    public class MainMenuPlugin : IMenu<MainViewContext>
+    {
+        private const string DIALOG_MENU = "DialogMenu";
+        private const string DIALOG_MENU_HEADER = "PilotMessageBox";
+
+        public void Build(IMenuBuilder builder, MainViewContext context)
+        {
+            var item = builder.AddItem(DIALOG_MENU, 1).WithHeader(DIALOG_MENU_HEADER);
+        }
+
+        public void OnMenuItemClick(string name, MainViewContext context)
+        {
+            if (name == DIALOG_MENU)
+            {
+                //PilotMessageBox.Show("Нет маршрутов для данного типа целевого объекта",
+                //                    "Внимание",
+                //                    MessageBoxButton.OK,
+                //                    MessageBoxImage.Warning);
+
+                PilotMessageBox.Show("Нет маршрутов для данного типа целевого объекта");
+            }
+        }
+    }
+}
